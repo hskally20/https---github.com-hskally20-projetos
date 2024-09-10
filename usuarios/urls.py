b186298228aeff5ff962febe83a32ea520bd35a1
HerculesSk <115839216+hskally20@users.noreply.github.com>
2024-09-10 02:54:45 +0000

    ajuste no logout

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,7 +5,7 @@ from .views import UsuarioCreate
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html' ), name='login'),
-    path('',auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('registrar/', UsuarioCreate.as_view(), name='registrar'),
    
 
