1e6b62ccaba663c7e64630f01a4c0063dfb82305
hercules <herculesskally81@gmail.com>
2024-10-11 17:45:02 -0300

    notificação teste 1

---
+++ consumers.py
@@ -0,0 +1,31 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class NotificacaoConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.paciente_id = self.scope['url_route']['kwargs']['paciente_id']
+        self.sala_grupo = f'notificacao_{self.paciente_id}'
+
+        # Junte-se ao grupo
+        await self.channel_layer.group_add(
+            self.sala_grupo,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Saia do grupo
+        await self.channel_layer.group_discard(
+            self.sala_grupo,
+            self.channel_name
+        )
+
+    # Método para receber notificações
+    async def receber_notificacao(self, event):
+        message = event['message']
+
+        # Envia mensagem para WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
