0c7bd52079edfda5d651344c244fa700457b1393
hercules <herculesskally81@gmail.com>
2024-10-31 10:58:25 -0300

    criando pagina admin

--- hospital/settings.py
+++ hospital/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'hospital.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'paginas' ,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
