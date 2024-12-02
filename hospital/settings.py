ed885fcc16bb521b9e05c42eb863f44c13f23dd0
sk <herculesskally81@gmail.com>
2024-12-02 17:07:44 -0300

    feito

--- hospital/settings.py
+++ hospital/settings.py
@@ -27,7 +27,7 @@ STATICFILES_DIRS = [
 SECRET_KEY = 'django-insecure-1s!kbdskouxxa7w)ova+2*f-e4mly&q^ij9v68)-qe8xmhtm(1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
