7f52840089b98ad50df8a1e05ef7373bcb415033
HerculesSk <115839216+hskally20@users.noreply.github.com>
2025-01-09 04:18:12 +0000

    hospedagem finalizada

--- hospital/settings.py
+++ hospital/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1s!kbdskouxxa7w)ova+2*f-e4mly&q^ij9v68)-qe8xmhtm(1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','https://virtual-touch.onrender.com']
 
 
 # Application definition
