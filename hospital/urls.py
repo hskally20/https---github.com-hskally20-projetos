029835f24406eb182684199fd0152070a6f150e0
HerculesSk <115839216+hskally20@users.noreply.github.com>
2025-01-09 03:58:59 +0000

    update

--- hospital/urls.py
+++ hospital/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('homepage.urls')),
     path('', include('paginas.urls')),
     path('', include('cadastros.urls')),
     path('', include('usuarios.urls')),
